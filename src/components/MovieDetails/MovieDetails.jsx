59f93f5c0b511b692b5a7b7110bb111f7679cbd4
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-30 17:12:09 -0500

    built out movie details page more

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import {useState} from 'react';
 import {useDispatch } from 'react-redux';
 import {useSelector} from "react-redux";
+import { useHistory } from 'react-router-dom';
 
 const movieDetails = () => {
+    //may be needed to go back to movie list page
+    const history = useHistory();
+    //selector and dispatch 
+    const details = useSelector ( store => store.details);
+    const dispatch = useDispatch();
+
+    // dispatch called 
+    //how do I know when to call a payload? 
+    const handleDetails = (event) => {
+        dispatch({type: 'SET_DETAILS', payload: event.target.value } )
+    }
+    return(
+        <>
+        </>
+    )
 
 }
 
