f749727a530cc791398a362fd784376e89ecf462
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-30 16:59:42 -0500

    added export default for ,ovie details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -2,3 +2,9 @@ import React from 'react';
 import {useState} from 'react';
 import {useDispatch } from 'react-redux';
 import {useSelector} from "react-redux";
+
+const movieDetails = () => {
+
+}
+
+export default movieDetails;
